b109c79c67f12ccea98b3b1c384be56778757e38
PIUS <relimoringa@gmail.com>
2023-09-26 10:01:58 +0300

    done

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import CheckConstraint
+from sqlalchemy.orm import validates
 
-
-
+app = Flask(__name__)
 db = SQLAlchemy()
 
 class Restaurant(db.Model):
@@ -13,6 +14,13 @@ class Restaurant(db.Model):
     address = db.Column(db.String, nullable=False)
     # Relationship definition
     pizzas = db.relationship('RestaurantPizza', back_populates='restaurant')
+
+    @validates('name')
+    def validate_name(self, key, name):
+        # Rule 2: Validation for name length (less than 50 characters)
+        if len(name) >= 50:
+            raise ValueError("Name must be less than 50 characters")
+        return name
 
     def serialize(self):
         return {
@@ -37,7 +45,8 @@ class Pizza(db.Model):
         return {
             'id': self.id,
             'name': self.name,
-            'ingredients': self.ingredients,
+            'address': self.address,
+            'pizzas': [pizza.serialize() for pizza in self.pizzas]
         }
 
 class RestaurantPizza(db.Model):
@@ -47,21 +56,23 @@ class RestaurantPizza(db.Model):
     pizza_id = db.Column(db.String, db.ForeignKey('pizzas.id'), nullable=False)
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
 
-    # Define the CheckConstraint for price
     price = db.Column(db.Float, nullable=False)
     __table_args__ = (
+        # Rule 1: CheckConstraint for price between 1 and 30
         CheckConstraint('price >= 1 and price <= 30', name='check_price_range'),
     )
+
+    @validates('price')
+    def validate_price(self, key, price):
+        # Rule 1: Validation for price between 1 and 30
+        if price < 1 or price > 30:
+            raise ValueError("Price must be between 1 and 30")
+        return price
 
     created_at = db.Column(db.DateTime, default=db.func.now())
     updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
     pizza = db.relationship('Pizza', back_populates='restaurants')
     restaurant = db.relationship('Restaurant', back_populates='pizzas')
 
-    def serialize(self):
-        return {
-            'id': self.id,
-            'pizza_id': self.pizza_id,
-            'restaurant_id': self.restaurant_id,
-            'price': self.price,
-        }
+if __name__ == '__main__':
+    app.run(debug=True)
